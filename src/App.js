a0da926669c0422c891fecd50cbb5e6a4e2d5c97
srjn45 <srajanpathak45@gmail.com>
2018-10-17 12:47:14 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import AutoColor from "./components/auto-color";
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       // <AutoColor backgroundColor={'#809070'}>
       //   <p>here is the para</p>
       // </AutoColor>
-      <AutoColor threshold={128} backgroundColor={'rgb(223, 252, 178)'}>
+      <AutoColor threshold={128} backgroundColor={'rgb(123, 252, 145)'}>
         <p>here is the para</p>
       </AutoColor>
     );
